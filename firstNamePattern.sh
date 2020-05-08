13224a143ab875c0a3c1df198b920dc1ea1b4c23
daminisalwe1234 <salwedamini2gmail.com>
2020-05-08 14:53:56 +0530

    first commit

---
+++ firstNamePattern.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+read -p "Enter first name" firstName
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo "Valid FirstName"
+else
+	echo "invalid FirstName"
+fi
+read -p "Enter last name" lastName
+lastNamePattern="^[[:upper:]]{1}[[:lower:]]{3,}$"
+if [[ $lastName =~ $lastNamePattern ]]
+then
+	echo "Valid LastName"
+else
+        echo "invalid LaststName"
+fi
